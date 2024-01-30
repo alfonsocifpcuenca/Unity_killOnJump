2e09f1aeeaba4f53bfb52578697bb197c0dd3d8b
alfonsocifpcuenca <alfonso.rodriguez@cifpcuenca.es>
2024-01-30 18:57:01 +0100

    Collision logic added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,14 +2,44 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    [SerializeField]
+    private Transform leftLimit;
+    [SerializeField]
+    private Transform rightLimit;
+
+    private bool moveLeft = true;
+
+    private void Update()
+    {
+        if (this.transform.position.x <= this.leftLimit.position.x)
+            moveLeft = false;
+
+        if (this.transform.position.x >= this.rightLimit.position.x)
+            moveLeft = true;
+
+        if (moveLeft)
+            this.transform.position = new Vector3(this.transform.position.x - (1 * Time.deltaTime), this.transform.position.y, this.transform.position.z);
+        else
+            this.transform.position = new Vector3(this.transform.position.x + (1 * Time.deltaTime), this.transform.position.y, this.transform.position.z);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (collision.GetContact(0).normal.y <= -0.9)
-            {
-                Debug.Log("Le matamos");
-            }
+            Destroy(collision.gameObject);
+        }
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+            collision.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 20, ForceMode2D.Impulse);
         }
     }
 }
